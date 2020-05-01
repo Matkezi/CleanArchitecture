e662962ae6b3f2782758477ede61f90d1d6cf6a7
Matkezi <matko.zurak@gmail.com>
2020-05-01 02:55:28 +0200

    Added untrusted skippers command and Imailer

---
+++ src/Application/Skippers/Commands/TrustedSkippers/UpdateTrustedSkippersCommand.cs
@@ -0,0 +1,52 @@
+﻿using CleanArchitecture.Application.Common.Exceptions;
+using CleanArchitecture.Application.Common.Interfaces;
+using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Infrastructure.Persistence.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Skippers.Commands.TrustedSkippers
+{
+    public partial class UpdateTrustedSkippersCommand : IRequest
+    {
+        public IEnumerable<string> Ids { get; set; }
+
+    }
+
+    public class UpdateTodoItemCommandHandler : IRequestHandler<UpdateTrustedSkippersCommand>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly ICurrentUserService _currentUserService;
+
+        public UpdateTodoItemCommandHandler(IApplicationDbContext context, ICurrentUserService currentUserService)
+        {
+            _context = context;
+            _currentUserService = currentUserService;
+        }
+
+        public async Task<Unit> Handle(UpdateTrustedSkippersCommand request, CancellationToken cancellationToken)
+        {
+            var charter = await _context.Charter.Include(c => c.UnTrustedSkippers).Include(c => c.TrustedSkippers).FirstAsync(x => x.Id == _currentUserService.UserId);
+            //var charter = await _context.Charter.FirstAsync(x => x.Id == _currentUserService.UserId);
+            request.Ids.ToList().ForEach(skipperId =>
+            {
+                if (!charter.TrustedSkippers.Select(x => x.SkipperID).Contains(skipperId))
+                {
+                    charter.TrustedSkippers.Add(new TrustedCharterSkipper { CharterID = charter.Id, SkipperID = skipperId });
+                }
+                charter.UnTrustedSkippers.RemoveAll(x => x.SkipperID == skipperId);
+
+            });
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+
+}
