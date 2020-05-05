b2729effe52b73e16d6f852275ef420c5237a6c0
Matkezi <matko.zurak@gmail.com>
2020-05-05 21:05:40 +0200

    Added charter get bookings

---
+++ src/Application/Bookings/Queries/CharterGetBookings/CharterGetBookingsQuery.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using CleanArchitecture.Application.Bookings.CommonModels;
+using CleanArchitecture.Application.Common.Interfaces;
+using CleanArchitecture.Application.Skippers.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SkipperBooking.Base.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Skippers.Queries.Availability
+{
+    public class CharterGetBookingsQuery : IRequest<IEnumerable<BookingModel>>
+    {
+        public BookingStatusEnum BookingStatus { get; set; }
+
+        public class Handler : IRequestHandler<CharterGetBookingsQuery, IEnumerable<BookingModel>>
+        {
+            private readonly IApplicationDbContext _context;
+            private readonly IMapper _mapper;
+            private readonly ICurrentUserService _currentUserService;
+
+            public Handler(IApplicationDbContext context, IMapper mapper, ICurrentUserService currentUserService)
+            {
+                _context = context;
+                _mapper = mapper;
+                _currentUserService = currentUserService;
+            }
+
+            public async Task<IEnumerable<BookingModel>> Handle(CharterGetBookingsQuery request, CancellationToken cancellationToken)
+            {
+                return await _context.Bookings
+                    .Include(b => b.Charter).Include(b => b.Boat)
+                    .Include(b => b.Skipper).Include(b => b.BookingHistories)
+                    .Where(b => b.Charter.Id == _currentUserService.UserId)
+                    .ProjectTo<BookingModel>(_mapper.ConfigurationProvider)
+                    .ToListAsync();
+            }
+
+        }
+    }
+}
--- src/Application/Common/Helpers/RandomUrl.cs
+++ src/Application/Common/Helpers/RandomUrl.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 
 namespace CleanArchitecture.Application.Common.Helpers
 {
-    public class RandomUrl
+    public static class RandomUrl
     {
         // List of characters and numbers to be used...  
-        private static List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
-        private static List<char> characters = new List<char>()
+        private static readonly List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+        private static readonly List<char> characters = new List<char>()
     {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
     'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B',
     'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
