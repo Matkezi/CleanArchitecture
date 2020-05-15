352119701c13e0b055689b24d0c1a6ffa7abb670
Matkezi <matko.zurak@gmail.com>
2020-05-15 13:17:53 +0200

    AppUser Developer role fix

---
+++ src/Infrastructure/Persistence/Configurations/AppUserConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SkipperAgency.Domain.Entities;
+
+namespace SkipperAgency.Infrastructure.Persistence.Configurations
+{
+    public class AppUserConfiguration : IEntityTypeConfiguration<AppUser>
+    {
+        public void Configure(EntityTypeBuilder<AppUser> builder)
+        {
+            builder.Property(e => e.Id)
+                .ValueGeneratedOnAdd();
+        }
+    }
+}
